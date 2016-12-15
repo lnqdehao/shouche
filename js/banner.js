70c9a7a378ddb5044fd4947ad16e67468cf57f0c
Johnny Le <1397169068@qq.com>
2016-12-15 19:39:46 +0800

    自行车三

---
+++ js/banner.js
@@ -0,0 +1,20 @@
+function 
+
+
+var bannerData = [
+	{
+		title : "广告",
+		href :"/ad",
+		url : "./image/banner1.jpg"
+	},
+	{
+		title : "大风车",
+		href :"/ad",
+		url : "./image/banner2.jpg"
+	},
+	{
+		title : "车牛",
+		href :"/ad",
+		url : "./image/banner3.jpg"
+	}
+];
